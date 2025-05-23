e90c27fa13b8e2f908042d4a861fde7eb473e0d1
anshikayadav11 <anshikayadav2917@gmail.com>
2025-05-23 23:09:36 +0530

    This is the first code in which i used m

---
+++ 2star.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m;
+    cout<<"Enter number of rows :";
+    cin>>m;
+    int n;
+    cout<<"Enter number of cols :";
+    cin>>n;
+    for(int i=1;i<=m;i++){
+        for (int i=1;i<=n;i++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
