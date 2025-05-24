86f642738d83432cb0e5b781a912e1a68a726b1e
anshikayadav11 <anshikayadav2917@gmail.com>
2025-05-24 23:09:01 +0530

    First comit

---
+++ 3newprint.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int fact(int n) {
+    int f = 1;
+    for (int i = 2; i <= n; i++) {
+        f *= i;
+    }
+    return f;
+}
+
+int nCr(int n, int r) {
+    return fact(n) / (fact(r) * fact(n - r));
+}
+
+int main() {
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    for (int i = 0; i <= n; i++) {
+        // spacing for center alignment
+        for (int j = 0; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        for (int j = 0; j <= i; j++) {
+            cout << nCr(i, j) << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
