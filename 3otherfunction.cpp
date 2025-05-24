254ff463e5b1fe911e9ddd90f0a873b35182319f
anshikayadav11 <anshikayadav2917@gmail.com>
2025-05-24 20:25:07 +0530

    First commit

---
+++ 3otherfunction.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void india() {
+    cout << "You are in India" << endl;
+}
+void usa() {
+    cout << "You are in USA" << endl;
+}
+int main() {
+    cout << "You are in main" << endl;
+    india();
+    usa();
+    return 0;
+}
