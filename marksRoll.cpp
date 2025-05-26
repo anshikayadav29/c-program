60feeb85f02c4f87dcd421bcd44e0080ceff70aa
anshikayadav11 <anshikayadav2917@gmail.com>
2025-05-26 20:04:30 +0530

    First commit

---
+++ marksRoll.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[4][2] ={{76,81},{13,76},{82,91},{88,90}};
+    int mx =INT_MIN;
+    for(int i=0;i<4;i++){
+        for(int j=0;j<2;j++){
+            mx =max(mx,arr[i][j]);
+        }
+    }
+    cout<<mx;
+}
