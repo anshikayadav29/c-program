66e62c17faace2c9bfeeeccd8ded56cdfb98b598
anshikayadav29 <anshikayadav2917@gmail.com>
2026-03-14 21:35:19 +0530

    Create Fulloperator.js

---
+++ Fulloperator.js
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int a=20,b=4;
+
+cout<<"Add "<<a+b<<endl;
+cout<<"Sub "<<a-b<<endl;
+cout<<"Mul "<<a*b<<endl;
+cout<<"Div "<<a/b<<endl;
+cout<<"Mod "<<a%b;
+}
