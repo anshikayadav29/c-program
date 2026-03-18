38bc2a14c0b67a28ab68d7a695673b36e704e948
anshikayadav29 <anshikayadav2917@gmail.com>
2026-03-19 00:57:27 +0530

    Create Checkbalanced.cpp

---
+++ Checkbalanced.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main(){
+string s;
+cin>>s;
+
+stack<char> st;
+
+for(char c:s){
+if(c=='(')
+st.push(c);
+else{
+if(st.empty()){
+cout<<"Not Balanced";
+return 0;
+}
+st.pop();
+}
+}
+
+if(st.empty())
+cout<<"Balanced";
+else
+cout<<"Not Balanced";
+}
