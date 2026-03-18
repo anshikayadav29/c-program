4a6cb0c97cfe5bf7bcbd7b585ebd638821231b6e
anshikayadav29 <anshikayadav2917@gmail.com>
2026-03-19 00:55:26 +0530

    Create Removeconcecutive.js

---
+++ Removeconcecutive.js
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+string s;
+cin>>s;
+
+string result="";
+result+=s[0];
+
+for(int i=1;i<s.length();i++){
+if(s[i]!=s[i-1])
+result+=s[i];
+}
+
+cout<<result;
+}
