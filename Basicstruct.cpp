a9acc6930129175610d326dd96117ee2c6b6cc02
anshikayadav29 <anshikayadav2917@gmail.com>
2026-03-20 02:54:16 +0530

    Create Basicstruct.cpp

---
+++ Basicstruct.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+struct Student {
+    int id;
+    string name;
+};
+
+int main() {
+    Student s;
+    s.id = 1;
+    s.name = "Anshika";
+
+    cout << s.id << " " << s.name;
+    return 0;
+}
