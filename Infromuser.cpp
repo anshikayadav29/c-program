244a148e173c08af0d92d1ec2d0389edc9810712
anshikayadav29 <anshikayadav2917@gmail.com>
2026-03-20 02:54:39 +0530

    Create Infromuser.cpp

---
+++ Infromuser.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+struct Student {
+    int id;
+    string name;
+};
+
+int main() {
+    Student s;
+    cin >> s.id >> s.name;
+
+    cout << s.id << " " << s.name;
+    return 0;
+}
