50094f1bc6e01b602a0419a5a9d14fccebedd7b9
anshikayadav29 <anshikayadav2917@gmail.com>
2026-03-21 16:45:41 +0530

    Create AvgMarks.cpp

---
+++ AvgMarks.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+struct Student {
+    int marks;
+};
+
+int main() {
+    Student s[3];
+    int sum = 0;
+
+    for(int i=0;i<3;i++) {
+        cin >> s[i].marks;
+        sum += s[i].marks;
+    }
+
+    cout << sum / 3;
+    return 0;
+}
