a59449d0799dedebeabf7e743b41f217611f4f5c
anshikayadav29 <anshikayadav2917@gmail.com>
2026-04-01 01:01:03 +0530

    Implement merging of two arrays into a third array

---
+++ Mergetwoarray.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[] = {1,2,3};
+    int b[] = {4,5,6};
+    int c[6];
+
+    int *p1 = a, *p2 = b, *p3 = c;
+
+    for(int i = 0; i < 3; i++) {
+        *(p3 + i) = *(p1 + i);
+        *(p3 + i + 3) = *(p2 + i);
+    }
+
+    for(int i = 0; i < 6; i++)
+        cout << c[i] << " ";
+
+    return 0;
+}
