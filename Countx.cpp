badb6cdb8bf684b6cc3d0051f7990becea368556
anshikayadav29 <anshikayadav2917@gmail.com>
2026-04-03 00:20:23 +0530

    Create Countx.cpp

---
+++ Countx.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int count(int n) {
+    if(n == 0) return 0;
+    return 1 + count(n / 10);
+}
+
+int main() {
+    cout << count(12345);
+    return 0;
+}
