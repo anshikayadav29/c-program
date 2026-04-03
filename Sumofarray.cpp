8c05b6d1a24c19f2242e53f13035db3fb84a8cf1
anshikayadav29 <anshikayadav2917@gmail.com>
2026-04-04 02:55:12 +0530

    Create Sumofarray.cpp

---
+++ Sumofarray.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int sum(int arr[], int n) {
+    if(n == 0) return 0;
+    return arr[n-1] + sum(arr, n-1);
+}
+
+int main() {
+    int arr[] = {1,2,3,4};
+    cout << sum(arr, 4);
+    return 0;
+}
