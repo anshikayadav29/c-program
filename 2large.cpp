275900255a5f183d6157f71d6b65e7c762ea8c2b
anshikayadav29 <anshikayadav2917@gmail.com>
2026-04-13 21:02:57 +0530

    Implement logic to find second largest number

---
+++ 2large.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {10, 20, 5, 40, 30};
+    int n = 5;
+
+    int largest = arr[0], second = -1;
+
+    for(int i = 0; i < n; i++) {
+        if(arr[i] > largest) {
+            second = largest;
+            largest = arr[i];
+        } else if(arr[i] > second && arr[i] != largest) {
+            second = arr[i];
+        }
+    }
+
+    cout << second;
+}
