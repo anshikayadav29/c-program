e32f9fa9f3f5417a9eca36776bca94c755c56971
anshikayadav29 <anshikayadav2917@gmail.com>
2026-04-14 14:06:24 +0530

    Create Findmissingno.cpp

---
+++ Findmissingno.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1,2,4,5};
+    int n = 5;
+
+    int sum = n*(n+1)/2;
+    int arrSum = 0;
+
+    for(int i=0;i<4;i++) arrSum += arr[i];
+
+    cout << sum - arrSum;
+}
