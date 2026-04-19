fcbc24e662ce52cda067521c19d437b1b21d4146
anshikayadav29 <anshikayadav2917@gmail.com>
2026-04-19 20:51:04 +0530

    Convert lowercase letters to uppercase in string

---
+++ Convertlowercase.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s = "hello";
+
+    for(char &c : s) {
+        if(c >= 'a' && c <= 'z')
+            c = c - 32;
+    }
+
+    cout << s;
+}
