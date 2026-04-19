1942002020265d24d85f049c6b71b12c8c8a0763
anshikayadav29 <anshikayadav2917@gmail.com>
2026-04-19 20:51:26 +0530

    Add duplicate finder in Findeuplicatexxx.cpp
    Implement a program to find and print duplicate elements in an array.

---
+++ Findeuplicatexxx.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1,2,2,3,4,4,5};
+
+    for(int i=1;i<7;i++) {
+        if(arr[i] == arr[i-1])
+            cout << arr[i] << " ";
+    }
+}
