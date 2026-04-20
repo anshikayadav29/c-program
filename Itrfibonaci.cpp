eb6573dce1aedcb07af84c56c92bf0e31b0aa65d
anshikayadav29 <anshikayadav2917@gmail.com>
2026-04-20 18:49:51 +0530

    Create Itrfibonaci.cpp

---
+++ Itrfibonaci.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 10, a = 0, b = 1;
+    for(int i = 0; i < n; i++) {
+        cout << a << " ";
+        int c = a + b;
+        a = b;
+        b = c;
+    }
+}
