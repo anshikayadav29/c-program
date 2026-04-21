74fe6a67267df78fd6a9b20440c42ff2bbd5872b
anshikayadav29 <anshikayadav2917@gmail.com>
2026-04-21 21:42:20 +0530

    Add Checksortedarray.cpp to check if array is sorted

---
+++ Checksortedarray.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1,2,3,4,5};
+    bool sorted = true;
+
+    for(int i = 1; i < 5; i++)
+        if(arr[i] < arr[i-1])
+            sorted = false;
+
+    cout << sorted;
+}
