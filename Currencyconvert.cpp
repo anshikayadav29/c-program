de8e6bd46bce1b942f597908cf76183713f2bfb2
anshikayadav17 <anshikayadav2917@gmail.com>
2026-05-27 09:12:40 +0530

    Create Currencyconvert.cpp

---
+++ Currencyconvert.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float usd, inr;
+
+    cout << "Enter USD: ";
+    cin >> usd;
+
+    inr = usd * 83;
+
+    cout << "INR: " << inr;
+
+    return 0;
+}
