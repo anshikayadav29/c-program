ff1ce46d492bba39fa6489f5a2eb8941af1b4f65
anshikayadav17 <anshikayadav2917@gmail.com>
2026-05-31 10:37:51 +0530

    Create Convertebenoddd.cpp

---
+++ Convertebenoddd.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10];
+    int even = 0, odd = 0;
+
+    for(int i = 0; i < 10; i++)
+        cin >> arr[i];
+
+    for(int i = 0; i < 10; i++) {
+        if(arr[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    cout << "Even: " << even << endl;
+    cout << "Odd: " << odd;
+}
