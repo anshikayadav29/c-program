78b88d33ec493658804479036f692921185b2cca
anshikayadav17 <anshikayadav2917@gmail.com>
2026-05-31 10:35:55 +0530

    Create Sortarrayindescending.cpp

---
+++ Sortarrayindescending.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5];
+
+    for(int i = 0; i < 5; i++)
+        cin >> arr[i];
+
+    for(int i = 0; i < 5; i++) {
+        for(int j = i + 1; j < 5; j++) {
+            if(arr[i] < arr[j])
+                swap(arr[i], arr[j]);
+        }
+    }
+
+    for(int i = 0; i < 5; i++)
+        cout << arr[i] << " ";
+}
